d99ae2cbe7d8c190e1234a11605d8a6804c3e9b9
Dylan C <dylanclowes@hotmail.com>
2023-06-22 15:05:49 -0700

    Added a Pokémon list
    Started my list of Pokémon and have added Bulbasaur, Charmander, and Squirtle

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,1 @@
-alert('Hello World');
-
-let favoriteFood = 'Steak';
-document.write(favoriteFood);
+let pokemonList = [{name: "Bulbasaur", height: 7, type ["grass","poison"]}{name: "Charmander", height: 6, type: "fire"}{name: "Squirtle", height: 5, type: "water"}]
