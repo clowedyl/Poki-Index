0a5bdb27c6018d718ed09b6cd3ffefe5c7ea10d0
Dylan C <dylanclowes@hotmail.com>
2023-06-22 21:55:04 -0700

    Cleaning up code
    Added missing commas and colons. As well as having a line for each Pokémon to clean it up.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,5 @@
-let pokemonList = [{name: "Bulbasaur", height: 7, type ["grass","poison"]}{name: "Charmander", height: 6, type: "fire"}{name: "Squirtle", height: 5, type: "water"}]
+let pokemonList = [
+    {name: "Bulbasaur", height: 7, type: ["grass","poison"]},
+    {name: "Charmander", height: 6, type: "fire"},
+    {name: "Squirtle", height: 5, type: "water"},
+]
