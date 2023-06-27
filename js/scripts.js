a8fc280f0a3e7961083102053655615a32dbd902
Dylan C <dylanclowes@hotmail.com>
2023-06-27 14:27:06 -0700

    Spaced out the Pokémon and added a loop
    Made the Pokémon easier to read name, height, and type.
    
    Also added a loop, a condition, and document write to make it all show on my pokedex

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,29 @@
+// Where I get my pokemon information from https://pokedex.org/#/pokemon/3
+
 let pokemonList = [
-    {name: "Bulbasaur", height: 7, type: ["grass","poison"]},
-    {name: "Charmander", height: 6, type: "fire"},
-    {name: "Squirtle", height: 5, type: "water"},
+    {
+        name: "Bulbasaur", 
+        height: 0.7, 
+        type: ["grass","poison"]
+    },
+    {
+        name: "Charmander", 
+        height: 0.6, 
+        type: "fire"
+    },
+    {
+        name: "Squirtle", 
+        height: 0.5, 
+        type: "water"
+    },
 ]
+for (let i=0; i < pokemonList.length; i++) { 
+        if (pokemonList[i].height > 0.6) {
+        document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' +' - Wow, that\'s big'+ '<br />');
+        document.write("<br />");
+        }
+        else {(pokemonList[i].height <= 0.6)
+            document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + '<br />');
+            document.write('<br />');
+    }
+}
