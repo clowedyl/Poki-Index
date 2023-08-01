a0e143311d344a122a1108676fa2dc8833f8a959
Dylan C <dylanclowes@hotmail.com>
2023-07-31 17:00:30 -0700

    Added Modals and UI patterns
    Added the modal to display on the main page and changed the show details to properly display all items and photos.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,62 @@
-// Where I get my pokemon information from https://pokedex.org/#/pokemon/3
 let pokemonRepository = (function() {
 
 let pokemonList = [];
 
 let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
+let modalContainer = document.querySelector('#modal-container');
+
+    function showModal(title, text, img) {
+    modalContainer.innerHTML = '';
+
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+    closeButtonElement.addEventListener('click', hideModal);
+
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = title;
+
+    let contentElement = document.createElement('p');
+    contentElement.innerText = text;
+
+    let imageElement = document.createElement("img");
+    imageElement.setAttribute("src", img);
+    imageElement.setAttribute("width", "96");
+    imageElement.setAttribute("height", "96");
+    imageElement.setAttribute("alt", "Pokemon");
+
+    modal.appendChild(closeButtonElement);                                                              
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    modal.appendChild(imageElement);
+    modalContainer.appendChild(modal);
+
+    modalContainer.classList.add('is-visible');
+  }
+
+    function hideModal() {
+    modalContainer.classList.remove('is-visible');
+  }
+
+    window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();
+    }
+  });
+
+    modalContainer.addEventListener('click', (e) => {
+    // Since this is also triggered when clicking INSIDE the modal container,
+    // We only want to close if the user clicks directly on the overlay
+    let target = e.target;
+    if (target === modalContainer) {
+      hideModal();
+    }
+  });
+  
     function add(pokemon) {
         if (
             typeof pokemon === "object" &&
@@ -24,8 +76,9 @@ let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
         let button = document.createElement('button');
         button.innerText = pokemon.name;
         button.classList.add('button-class');
-        button.addEventListener('click', function (showDetails) {
-             console.log(pokemon);
+
+        button.addEventListener('click', function () {
+             showDetails(pokemon);
         });
         listItem.appendChild(button);
         pokemonList.appendChild(listItem);
@@ -58,9 +111,9 @@ let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
         });
     }
 
-    function showDetails(pokemon) {
-        loadDetails(pokemon).then(function () {
-            console.log(pokemon);
+    function showDetails(item) {
+        loadDetails(item).then(function () {
+            showModal(item.name, "Height:" + item.height, item.imageUrl);
         })
     }
     return {
@@ -73,7 +126,7 @@ let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
     };
 
     
- })();
+})();
 
  pokemonRepository.loadList().then(function() {
 
